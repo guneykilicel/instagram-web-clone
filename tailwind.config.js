d62f4e09f0f56402e5acddae354f5d8015b26aa6
guneykilicel <80408285+guneykilicel@users.noreply.github.com>
2022-08-28 18:09:22 +0300

    1. ders kodları

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        brand: '#0095f6',
+        facebook: '#385185',
+        link: '#00376b'
+      },
+      backgroundImage: {
+       
+        'logo-pattern': "url('../src/assets/images/instaBG.png')",
+    
+      }
+    }
+  },
+  plugins: [],
+}
